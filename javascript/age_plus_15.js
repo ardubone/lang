d94093e69d063aabb1aaa324b0a34d66d658873d
ardubone <bone_666@mail.ru>
2022-12-06 13:40:40 +0300

    Create age_plus_15.js

---
+++ javascript/age_plus_15.js
@@ -0,0 +1,7 @@
+function plus15(currentAge)  // установка функции
+{
+  let age = 51;
+  return age + 15 ; // возврат с данными
+}
+let newAge = plus15(age) ; // вызов функции с параметром age
+alert ('А через 15 лет будет ' + newAge ); // вывод текста + данные
